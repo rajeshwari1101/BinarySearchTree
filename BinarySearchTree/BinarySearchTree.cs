aa950b323a26acaadaf2f4c31db829ed7eec557d
rajeshwari1101 <rajeshwaripatil1101@gmail.com>
2022-03-06 20:34:33 +0530

    Creating binary search tree

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree<T> where T : IComparable
+    {
+        //Variables
+        public T data;
+        public BinarySearchTree<T> left;
+        public BinarySearchTree<T> right;
+
+        //Creating constructor of class to initialize 
+        public BinarySearchTree(T Data)
+        {
+            this.data = Data;
+            left = null;
+            right = null;
+        }
+
+        //Creating method to insert  items
+        public void Insert(T item)
+        {
+            if (this.data.CompareTo(item) > 0)
+            {
+                if (left != null)
+                    left.Insert(item);
+                else
+                    left = new BinarySearchTree<T>(item);
+            }
+            else if (right != null)
+            {
+                right.Insert(item);
+            }
+            else
+                right = new BinarySearchTree<T>(item);
+
+        }
+
+        //Creating method to display the nodes
+        public void Display()
+        {
+
+            if (left != null)
+                left.Display();
+
+            Console.Write(data + " ");
+
+            if (right != null)
+                right.Display();
+        }
+
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating object of class
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            Console.WriteLine("Nodes in Binary search tree = ");
+            binarySearch.Display();
+            Console.ReadKey();
+        }
+    }
+}
